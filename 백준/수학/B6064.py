22ea4de1bc208b6799921caa91c95cbb16db7938
smart-sangmin <sangmins930@gmail.com>
2022-01-27 22:49:39 +0900

    Initial commit

---
+++ 백준/수학/B6064.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T):
+    N, M, x, y = map(int, input().split())
+    flag = -1
+    day = x
+    while M * N > day:
+        if day % M == y or (day % M == 0 and M == y):
+            flag = day
+            break
+        day += N
+    print(flag)
