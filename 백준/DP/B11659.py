462f0c62189cc1d77405fc6af17bd19dfb3e93ba
smart-sangmin <sangmins930@gmail.com>
2022-01-28 16:40:55 +0900

    Initial commit

---
+++ 백준/DP/B11659.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+num_list = list(map(int, input().split()))
+dp = [0]
+for i in range(len(num_list)):
+    dp.append(dp[-1] + num_list[i])
+for _ in range(M):
+    i, j = map(int, input().split())
+    print(dp[j] - dp[i - 1])
