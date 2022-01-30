a4ede61f473ebb2b7a99a9ba88d3dd0b763b4c1d
smart-sangmin <sangmins930@gmail.com>
2022-01-30 16:17:11 +0900

    Initial commit

---
+++ 백준/자료구조/B11286.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+N = int(input())
+q = []
+for _ in range(N):
+    num = int(input())
+    if num == 0:
+        if q:
+            print(heapq.heappop(q)[1])
+        else:
+            print(0)
+    elif num > 0:
+        heapq.heappush(q, (num, num))
+    else:
+        heapq.heappush(q, (-num, num))
