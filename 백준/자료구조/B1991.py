87c712de91723a8ce2e91c3bdc707a0495093b94
smart-sangmin <sangmins930@gmail.com>
2022-02-04 23:53:55 +0900

    Initial commit

---
+++ 백준/자료구조/B1991.py
@@ -0,0 +1,36 @@
+N = int(input())
+tree = {}
+for _ in range(N):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+
+def preorder(root):
+    if root == '.':
+        return
+    print(root, end="")
+    preorder(tree[root][0])
+    preorder(tree[root][1])
+
+
+def inorder(root):
+    if root == '.':
+        return
+    inorder(tree[root][0])
+    print(root, end="")
+    inorder(tree[root][1])
+
+
+def postorder(root):
+    if root == '.':
+        return
+    postorder(tree[root][0])
+    postorder(tree[root][1])
+    print(root, end="")
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
