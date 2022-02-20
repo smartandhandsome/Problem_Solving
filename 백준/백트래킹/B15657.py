f5bf8df161e9ffd4f6e17f687c6538ec3e460315
smart-sangmin <sangmins930@gmail.com>
2022-02-20 16:23:07 +0900

    Initial commit

---
+++ 백준/백트래킹/B15657.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+line = []
+
+
+def function(x):
+    global line
+    if x == 0:
+        print(*line)
+        return
+    for num in nums:
+        if not line or num >= line[-1]:
+            line.append(num)
+            function(x - 1)
+            line.remove(num)
+
+
+function(M)
